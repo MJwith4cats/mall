753af63ad05d9d85281df100c9e231d294dac65e
Woo Minji <life.exhib@gmail.com>
2024-11-19 17:53:42 +0900

    security 적용, 리덕스 시작

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+
+
+//configureStore()에서 생성되는 Store를 애플리케이션에 적용하기 위해서 index.js 파일을 수정
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+        <App/>
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
